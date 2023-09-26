f0d93fd0e78684b2d24259902810547687947e62
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-09-26 19:45:14 +0200

    Create NavBar.js

---
+++ frontend/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className='w-100' style={{ position: 'absolute' }}>
+            <div className="bg-dark p-4 mb-2">
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
