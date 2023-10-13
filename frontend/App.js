73f1df995f7b8bd2256adaff526a826a0065ab59
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-10-13 18:00:28 +0200

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import Login from "./User/Login";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Signup from './User/Signup';
-import StartPage from './StartPage';
 import PartElections from './Candidate/PartElections';
 import CandidateProfile from './Candidate/CandidateProfile';
 import Logout from './Components/Logout';
@@ -10,23 +8,26 @@ import Home from './User/Home';
 import UpdateEmail from './User/UpdateEmail';
 import UpdatePassword from './User/UpdatePassword';
 import VotingPage from './Candidate/VotingPage';
+import StartPage from './Components/StartPage';
+import { UserInfo } from "./User/Contexts/userContext";
 
 function App() {
+
   return (
-      <BrowserRouter>
-        <Routes>
+    <UserInfo>
+      <Routes>
         <Route path='/' element={<StartPage />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/UpdatePassword' element={<UpdatePassword />} />
-          <Route path='/UpdateEmail' element={<UpdateEmail />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/PartElections' element={<PartElections />} />
-          <Route path='/candidate/:id' element={<CandidateProfile />} />
-          <Route path='/VotingPage' element={<VotingPage />} />
-          <Route path='/Logout' element={<Logout />} />
-        </Routes>
-      </BrowserRouter>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/UpdatePassword' element={<UpdatePassword />} />
+        <Route path='/UpdateEmail' element={<UpdateEmail />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/PartElections' element={<PartElections />} />
+        <Route path='/candidate/:id' element={<CandidateProfile />} />
+        <Route path='/VotingPage' element={<VotingPage />} />
+        <Route path='/Logout' element={<Logout />} />
+      </Routes>
+    </UserInfo>
   );
 }
 export default App;
