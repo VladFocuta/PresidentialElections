43f0ea21d8d1cb4afd1ebc6ba18223a22a373046
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-10-13 17:59:57 +0200

    Update VotingPage.js

--- frontend/candidate/VotingPage.js
+++ frontend/candidate/VotingPage.js
@@ -1,30 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useAuth } from '../User/Contexts/userContext';
 
 function VotingPage() {
-    const [auth, setAuth] = useState(false);
-    const navigate = useNavigate();
+    const { user, loggedIn, setLoggedIn } = useAuth();
     const [checkBoxes, setCheckBoxes] = useState({});
     const [errorMessage, setErrorMessage] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-
-    axios.defaults.withCredentials = true;
-    useEffect(() => {
-        axios.get('http://localhost:8081')
-            .then(res => {
-                if (res.data.Status === "Succes") {
-                    setAuth(true);
-                    setUserEmail(res.data.email);
-                    console.log(res.data);
-                } else {
-                    setAuth(false)
-                }
-            })
-            .catch(err => console.log(err))
-    }, [navigate]);
-
+    const navigate = useNavigate();
     const [candidates, setCandidates] = useState([]);
+
     useEffect(() => {
         axios.get('http://localhost:8081/getCandidates/list')
             .then(res => {
@@ -33,12 +18,15 @@ function VotingPage() {
             .catch(err => console.log(err));
     }, []);
 
-    const handleLogOut = () => {
+    const userLogOut = () => {
         axios.get('http://localhost:8081/user/logout')
             .then(res => {
+                localStorage.removeItem('authToken');
+                setLoggedIn(false);
                 navigate('/');
-            }).catch(err => console.log(err));
-    };
+            })
+            .catch(err => console.log(err));
+    }
 
     const handleCheckBoxChange = (id, email) => {
         setCheckBoxes(prevState => ({
@@ -60,17 +48,12 @@ function VotingPage() {
         }
 
         axios.put('http://localhost:8081/user/updateVotes', { email: selectedEmail })
-            .then(res => {
-                if (res.data.updated) {
-                    handleLogOut();
-                }
-            })
             .catch(err => console.log(err));
 
-        axios.put('http://localhost:8081/user/hasVoted', { email: userEmail })
+        axios.put('http://localhost:8081/user/hasVoted', { email: user.email })
             .then(res => {
                 if (res.data.updated) {
-                    handleLogOut();
+                    userLogOut();
                 } else {
                     setErrorMessage('There was an error while trying to vote, please try again!')
                 }
@@ -79,14 +62,14 @@ function VotingPage() {
     };
 
     return (
-        <div style={{}}>
+        <div>
             <Link className='bg-dark text-white p-3 mb-2 w-100' to='/' style={{ position: 'absolute', height: '7%', textDecoration: 'none' }}>
                 <div className="elections-container" style={{ fontSize: '30px' }}>
                     <strong>Elect</strong>ions
                 </div>
             </Link>
             {
-                auth ? (
+                loggedIn ? (
                     <div style={{ position: 'absolute', top: '15%', left: '36%' }}>
                         <div className='d-flex justify-content-center mt-3' style={{ maxWidth: '100%', borderRadius: '10%', border: '5px solid black', backgroundColor: 'grey' }}>
                             <h1 style={{ color: 'white' }} >Candidates List:</h1>
