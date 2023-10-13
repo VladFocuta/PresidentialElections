0890a6fbe157cbc3f807324602df078ab36a7595
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-10-13 18:00:45 +0200

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -5,10 +5,11 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
